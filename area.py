982ee33549d059b78083f92fef68b568ce8bb7a9
bb-barricade <brandon.barr@barricadeconsulting.com>
2026-06-17 11:11:47 -0400

    Create area.py

---
+++ area.py
@@ -0,0 +1,38 @@
+import pi from math
+
+print("==================")
+print("Area Calculater 📐")
+print("==================")
+print("")
+print("1\) Triangle")
+print("2\) Rectangle")
+print("3\) Square")
+print("4\) Circle")
+print("5\) Quit")
+print("")
+
+shape = input('Which shape: ')
+print("")
+
+if shape == 1:
+  base = input('Base: ')
+  height = input('Height: ')
+  print("")
+  area = base * height / 2
+elif shape == 2 or shape == 3:
+  base = input('Base: ')
+  height = input('Height: ')
+  print("")
+  area = base * height
+elif shape == 4:
+  radius = inpu('Radius: ')
+  p = pi()
+  area = p * radius * radius
+else
+  exit()
+
+print(f"The area is {area}")
+
+
+
+  
